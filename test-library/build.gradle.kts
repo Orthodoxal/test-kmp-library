0b61e66320e5ec51a1eb2afc413330e27b4f61bc
Orthodoxal <dimariktron@mail.ru>
2024-10-03 15:59:13 +0400

    up version

--- test-library/build.gradle.kts
+++ test-library/build.gradle.kts
@@ -55,7 +55,7 @@ mavenPublishing {
     coordinates(
         groupId = "io.github.orthodoxal",
         artifactId = "test-library",
-        version = "0.0.1"
+        version = "0.0.2"
     )
 
     pom {
